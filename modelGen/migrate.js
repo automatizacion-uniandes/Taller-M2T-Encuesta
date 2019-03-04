29cad1eab34ad3676ea5c01bfc49e2c102d565d4
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2019-03-03 23:38:52 -0500

    First Commit

---
+++ modelGen/migrate.js
@@ -0,0 +1,124 @@
+
+//Agregar preguntas
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 0,
+			pregunta: ¿Cual es su nombre?,
+			modulo: 0,
+			espreguntainicial: 1,
+			tipo: 0,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 1,
+			pregunta: ¿Cual es su edad?,
+			modulo: 0,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 2,
+			pregunta: Cual es su estado civil?,
+			modulo: 0,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 3,
+			pregunta: Cual es su grado de escolaridad?,
+			modulo: 0,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 4,
+			pregunta: Hace cuanto vive con su pareja?,
+			modulo: 0,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 5,
+			pregunta: Cuales son sus ingresos mensuales?,
+			modulo: 1,
+			espreguntainicial: 1,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 6,
+			pregunta: Cuantos trabajos tiene?,
+			modulo: 1,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 7,
+			pregunta: Usted es trabajador independiente?,
+			modulo: 1,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 8,
+			pregunta: Su/s contrato/s es/son,
+			modulo: 1,
+			espreguntainicial: 0,
+			tipo: 1,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 9,
+			pregunta: Cotiza salud y pension?,
+			modulo: 1,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 10,
+			pregunta: Cuantos años estudio en la universidad?,
+			modulo: 2,
+			espreguntainicial: 1,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 11,
+			pregunta: Estudio en una universidad?,
+			modulo: 2,
+			espreguntainicial: 0,
+			tipo: 1,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 12,
+			pregunta: Usted termino todos sus estudios?,
+			modulo: 2,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 13,
+			pregunta: Usted Pago?,
+			modulo: 2,
+			espreguntainicial: 0,
+			tipo: 2,
+			}
+			
+			ENCUESTA.insertPregunta{
+			idpregunta: 14,
+			pregunta: Cual es el nombre de su universidad?,
+			modulo: 2,
+			espreguntainicial: 0,
+			tipo: 0,
+			}
+
+
